d23cbdc98ac297f0366418485220e25aca1b8bee
Your Name <ura2rist@gmail.com>
2022-06-07 17:00:50 +0300

    Модули для css and html

--- src/script/main.js
+++ src/script/main.js
@@ -1,1 +1,1 @@
-alert(1)
+import '../style/style.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,35 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
-const filename = (ext) => isDev ? `[name].${exr}` : `[name].[contenthash].${ext}`;
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: './script/main.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            minify: isProd
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: './style/style.css'
+        })
+    ],
     module: {
         rules: [
-            { test: /\.css$/, use: ['style-loader','css-loader'] },
+            { test: /\.css$/i, use: ['style-loader', MiniCssExtractPlugin.loader,'css-loader'] },
             { test : /\.(js)$/, use: 'babel-loader' }
         ]
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: `./js/${filename('js')}`
-    },
-    plugins: [
-        new HtmlWebpackPlugin()
-    ]
+    }
 }
