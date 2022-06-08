ecd2011e4c2b017c07464f959d38193c9697d58c
ura2rist <ura2rist@gmail.com>
2022-06-08 22:58:30 +0300

    Работа с картинками

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
@@ -20,6 +21,12 @@ module.exports = {
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
             filename: './style/style.css'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, 'src/assets'),
+                    to: path.resolve(__dirname, 'dist') }
+            ]
         })
     ],
     module: {
@@ -34,7 +41,16 @@ module.exports = {
                     'css-loader'
                 ]
             },
-            { test : /\.(js)$/, use: 'babel-loader' }
+            { test: /\.html/, use: 'html-loader' },
+            { 
+                test: /\.(?:|gif|png|jpg|jpeg|svg)$/, 
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[path][name].[ext]',
+                    }
+                }]
+            }
         ]
     },
     output: {
